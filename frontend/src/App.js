a05d18bebbe540dc055a92dac136eccf3e9f7b87
LEONARDO FRANCA BESSA <leonardo.bessa@equatorialservicos.com.br>
2024-06-21 17:27:53 -0300

    initial react

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './assets/styles/globals.css'
+import FirstHeader from './components/headers/FirstHeader/FirstHeader';
+
+function App() {
+  return (
+    <div className="App">
+      <FirstHeader phone="(98) 984190219"  email="tkmultimarcas@gmail.com"/>
+    </div>
+  );
+}
+
+export default App;
