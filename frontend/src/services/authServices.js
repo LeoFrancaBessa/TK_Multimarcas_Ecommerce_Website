42984163ca721f8c713365a31049d1407d62035f
LEONARDO FRANCA BESSA <leonardo.bessa@equatorialservicos.com.br>
2024-06-26 13:46:37 -0300

    alter auth

--- frontend/src/services/authServices.js
+++ frontend/src/services/authServices.js
@@ -1,4 +1,5 @@
-const API_URL = 'http://127.0.0.1:8000/auth/api/token/'
+const API_URL = 'http://127.0.0.1:8000/auth/api/login/'
+const API_LOGOUT = 'http://127.0.0.1:8000/auth/api/logout/'
 
 export async function login(username, password) {
     const response = await fetch(API_URL, {
@@ -6,9 +7,22 @@ export async function login(username, password) {
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({username, password})
+        body: JSON.stringify({username, password}),
+        credentials : "include"
     });
-    
+
+    const data = await response.json();
+    return data;
+}
+
+export async function logout() {
+    const response = await fetch(API_LOGOUT, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
     const data = await response.json();
     return data;
 }
